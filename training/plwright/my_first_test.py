8ece2241572830fd420a9975828caadf9f4d88f4
Matan <matanhtzvi@gmail.com>
2026-01-21 18:43:24 +0200

    first commit test

--- training/plwright/my_first_test.py
+++ training/plwright/my_first_test.py
@@ -1,21 +1,14 @@
 from playwright.sync_api import sync_playwright
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
-    page.goto("http://www.ebay.com")
-
-    search = page.locator("[id='gh-ac']")
-    search.click()
-    search.clear()
-    search.fill("Phone")
-    search_button = page.locator("[id='gh-search-btn']")
-    search_button.click()
-    # page.keyboard.press('Enter')
-    current_url = page.url
-    assert page.url.__contains__("ebay"),"page URL did not contains Ebay after search"
-
-
+    page.goto("https://www.ebay.com")
+    search_menu = page.locator("[id='gh-ac']")
+    search_menu.click()
+    search_menu.fill("Camera")
+    page.keyboard.press("Enter")
+    page.close()
     browser.close()
     print ("Test End")
 
